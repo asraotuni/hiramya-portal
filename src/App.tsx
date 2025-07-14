d74679817ed854468bf5bd5f7dd93fc4dcb0a5f1
SrinivasaRao A <asraotuni@users.noreply.github.com>
2025-07-14 15:43:24 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ function App() {
     },
     {
       category: 'Security Tools',
-      items: ['HashiCorp Vault', 'AWS IAM', 'OWASP ZAP', 'SonarQube']
+      items: [ 'AWS IAM', 'Okta', 'SonarQube']
     }
   ];
 
